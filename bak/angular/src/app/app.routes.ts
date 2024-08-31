245a2725cc6069417d81aff92226348467ebf073
GeorgeMSchaffer <gschaffer@gmail.com>
2024-08-31 11:46:51 -0700

    Removed old projects adding DotNet, Java, and Kotlin backends

--- bak/angular/src/app/app.routes.ts
+++
@@ -1,5 +0,0 @@
-import { Routes } from '@angular/router';
-import { IntervalsComponent } from './modules/intervals/intervals.component';
-export const routes: Routes = [
-    {path:'intervals',component:IntervalsComponent}
-];
